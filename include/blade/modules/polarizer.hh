f4f7048f1110739016eb6084bf978a7ba853fbbd
Luigi Cruz <github@luigi.ltd>
2024-08-02 16:04:53 -0700

    Add missing line in `Polarizer` module configuration.

--- include/blade/modules/polarizer.hh
+++ include/blade/modules/polarizer.hh
@@ -14,6 +14,7 @@ class BLADE_API Polarizer : public Module {
     struct Config {
         POL inputPolarization = POL::XY;
         POL outputPolarization = POL::LR;
+
         U64 blockSize = 512;
     };
 
